476f4d566c2c136c990adfcd67fa0c2cebdc927d
kasyan1337 <154789069+kasyan1337@users.noreply.github.com>
2024-01-01 05:20:12 +0800

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
-def to_upper_case(text):
-    return text.upper()
+def to_upper_case(s):
+    """
+    Converts a given string to uppercase.
+    
+    Parameters:
+    s (str): String to be converted.
+
+    Returns:
+    str: String in uppercase.
+    """
+    return s.upper()
