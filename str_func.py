a8cbefab5466afc3ce565b5ef01b763c1760e754
kasyan1337 <tristokorun@gmail.com>
2024-01-01 05:22:35 +0800

    Added function capitalize_first_letters

--- str_func.py
+++ str_func.py
@@ -9,3 +9,16 @@ def to_upper_case(s):
     str: String in uppercase.
     """
     return s.upper()
+
+
+def capitalize_first_letters(s):
+    """
+    Capitalizes the first letter of each word in the string.
+
+    Parameters:
+    s (str): String to be capitalized.
+
+    Returns:
+    str: String with the first letter of each word capitalized.
+    """
+    return s.title()
