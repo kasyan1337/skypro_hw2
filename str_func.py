607b4304eaf72499e5f31912921bb549ad57c72b
kasyan1337 <154789069+kasyan1337@users.noreply.github.com>
2024-01-07 03:17:28 +0800

    Update str_func.py
    Add docstring!

--- str_func.py
+++ str_func.py
@@ -1,11 +1,23 @@
 def to_upper_case(s):
     """
-    Converts a given string to uppercase.
+    Converts a given string to uppercase!
     
     Parameters:
-    s (str): String to be converted.
-
+    s (str): String to be converted!
     Returns:
-    str: String in uppercase.
+    str: String in uppercase!
     """
     return s.upper()
+
+
+def capitalize_first_letters(s):
+    """
+    Capitalizes the first letter of each word in the string!
+
+    Parameters:
+    s (str): String to be capitalized.
+
+    Returns:
+    str: String with the first letter of each word capitalized!
+    """
+    return s.title()
